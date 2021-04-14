45ad2f97a3c4ea38f79c516b45aa9d8325c54f5a
nicolassistema <nicolas.letticugna@gmail.com>
2021-04-13 23:35:06 -0300

    buen dia profesor, tiene ganas de dar clase?

---
+++ Ejercicio 33 en clase/Entidades/Libro.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Libro
+    {
+
+        List<string> paginas = new List<string>();
+
+        public string this[int i]
+        {
+
+            get
+            {
+                if (i < this.paginas.Count)
+                {
+                    return this.paginas[i];
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            set
+            {
+                if (i < this.paginas.Count)
+                {
+                    this.paginas[i] = value;
+                }
+                else
+                {
+                    paginas.Add(value);
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ Ejercicio 33 en clase/Test/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+
+namespace Entidades
+{
+    class Program
+    {
+        private static void MostrarIndices(Libro indexado, int cantidadIndices)
+        {
+            for (int i = 0; i < cantidadIndices; i++)
+            {
+                Console.WriteLine($"{indexado[i]}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Libro harryPotter = new Libro();
+
+            harryPotter[0] = "Capitulo 1";
+            harryPotter[1] = "Capitulo 2";
+            harryPotter[2] = "Capitulo 3";
+
+            MostrarIndices(harryPotter, 4);
+
+            harryPotter[0] = "Capitulo 0";
+            harryPotter[1] = "Capitulo 1.1";
+
+            MostrarIndices(harryPotter, 2);
+
+            Console.ReadKey();
+        }
+    }
+}
