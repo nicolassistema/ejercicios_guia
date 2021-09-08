48adb8f27ed9f1bac0de71faf25a9b343baa6224
nicolas <nicolas.letticugna@gmail.com>
2021-09-07 22:58:50 -0300

    actualizando directorio

---
+++ 2°Cuatrimestre/Ejercicios segundo cuatrimestre/Introduccion a .NET y C#/Recibo de sueldo/Ejercicio I07 - Recibo de sueldo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_I07___Recibo_de_sueldo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
